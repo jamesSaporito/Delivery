22cf049f2045d06cb61c39ff0f3bf998c49a3069
James Saporito <jamesasaporito@gmail.com>
2017-01-22 01:35:03 -0500

    Created classes, got rid of lists, and everything else...

--- responder.py
+++ responder.py
@@ -11,74 +11,93 @@ import MySQLdb
 SUBJECT = "Delivery!!!"
 BODY = "OP *may* have delivered! Here is the link to the thread: "
 
-#DATABASE CONNECTION
-db = MySQLdb.connect(host=config.host, user=config.username, passwd=config.password, db=config.db)
-db.autocommit(True)
-cur = db.cursor()
+#Used to connect to the database
+class DatabaseConnection():
+    db = None
+    cur = None
 
-#================================= FUNCTIONS ===================================
+    def __init__(self):
+        self.db = MySQLdb.connect(host=config.host, user=config.username, passwd=config.password, db=config.db)
+        self.cur = self.db.cursor()
 
-#Searches recorded threads to see if OP has delivered or not
-def search_for_delivery(reddit):
-    cur.execute("SELECT DISTINCT username, comment_id FROM Reddit_Threads")
-    rows = cur.fetchall()   #Gets data from SQL statement above and goes through it one by one
+class DeliverySearch():
+    database = DatabaseConnection()
 
-    #data[0] is the username and data[1] is the comment ID
-    for data in rows:
-        try:
-            comments = reddit.comment(id=data[1])
-            comments.refresh()
-            for comment in comments.replies.list():
-                if comment.author == data[0]:
-                    find_subscribers(reddit)
-                else:
-                    continue
-        except:
-            submission = reddit.submission(id=data[1])
-            for comment in submission.comments.list():
-                if comment.author == data[0]:
-                    find_subscribers(reddit)
-                else:
-                    continue
+    def __init__(self, reddit):
+        self.reddit = reddit
+        self.search_for_delivery()
+        self.database.db.commit()
+
+    #Searches recorded threads to see if OP has delivered or not
+    def search_for_delivery(self):
+        self.database.cur.execute("SELECT DISTINCT username, comment_id FROM Reddit_Threads")
+        rows = self.database.cur.fetchall()   #Gets data from SQL statement above and goes through it one by one
+
+        for data in rows:
+            try:
+                comments = self.reddit.comment(id=data[1])
+                comments.refresh()
+
+                for comment in comments.replies.list():
+                    if comment.author == data[0]:
+                        self.find_subscribers(self.reddit)
+                    else:
+                        continue
+            except Exception as e:
+                print str(e)
+                submission = self.reddit.submission(id=data[1])
+
+                for comment in submission.comments.list():
+
+                    if comment.author == data[0]:
+                        self.find_subscribers(self.reddit)
+                    else:
+                        continue
 
 
-def find_subscribers(reddit):
-    cur.execute("SELECT DISTINCT subscriber, thread_id, comment_id FROM Reddit_Threads")
-    rows = cur.fetchall()
-    for data in rows:
-        url = reddit.submission(id=data[1])
+    def find_subscribers(self, reddit):
+        self.database.cur.execute("SELECT DISTINCT subscriber, thread_id, comment_id FROM Reddit_Threads")
+        rows = self.database.cur.fetchall()
+        for data in rows:
+            url = self.reddit.submission(id=data[1])
 
-        if check_replied_to(data[0], data[2]) == False:
-            message_op_delivered(reddit, url.shortlink, data[0])
-            update_databases(reddit, data[0], data[1], data[2])
+            if self.check_replied_to(data[0], data[2]) == False:
+                self.message_op_delivered(reddit, url.shortlink, data[0])
+                self.update_databases(reddit, data[0], data[1], data[2])
 
-#Sends messages that OP delivered
-def message_op_delivered(reddit, shortlink, subscriber): #Select all repeats of the id if reply is found
-    redditor = praw.models.Redditor(reddit, subscriber)
-    test = redditor.message(SUBJECT, BODY + shortlink)
-    print("***** Message sent *****")
+    #Sends messages that OP delivered
+    def message_op_delivered(self, reddit, shortlink, subscriber): #Select all repeats of the id if reply is found
+        redditor = praw.models.Redditor(reddit, subscriber)
+        redditor.message(SUBJECT, BODY + shortlink)
+        print("***** Message sent *****")
 
-def update_databases(reddit, subscriber, thread_id, comment_id):
-    cur.execute("INSERT INTO Replied_To (subscriber, thread_id, comment_id) VALUES (%s, %s, %s)", (subscriber, thread_id, comment_id)) #insert this info into the Responded_To table
-    cur.execute("DELETE FROM Reddit_Threads WHERE subscriber = %s and comment_id = %s", (subscriber, comment_id))
+    def update_databases(self, reddit, subscriber, thread_id, comment_id):
+        self.database.cur.execute("DELETE FROM Reddit_Threads WHERE subscriber = %s and comment_id = %s", (subscriber, comment_id))
+        self.database.cur.execute("INSERT INTO Replied_To (subscriber, thread_id, comment_id) VALUES (%s, %s, %s)", (subscriber, thread_id, comment_id)) #insert this info into the Responded_To table
 
-#Check to see if someone has already been replied to in the database
-def check_replied_to(comment_author, thread_id):
-    cur.execute("SELECT * FROM Replied_To WHERE subscriber = %s and comment_id = %s", (comment_author, thread_id))
+    #Check to see if someone has already been replied to in the database
+    def check_replied_to(self, comment_author, thread_id):
+        self.database.cur.execute("SELECT * FROM Replied_To WHERE subscriber = %s and comment_id = %s", (comment_author, thread_id))
 
-    if cur.rowcount == 0:
-        return False
-    else:
-        return True
+        if self.database.cur.rowcount == 0:
+            return False
+        else:
+            return True
+
+#Create the tables if it doesn't already exist
+def create_tables():
+    connection = DatabaseConnection()
+    connection.cur.execute("CREATE TABLE IF NOT EXISTS Reddit_Threads (username varchar(50), subscriber varchar(50), thread_id varchar(10), comment_id varchar(10), PRIMARY KEY (subscriber, comment_id));")
+    connection.cur.execute("CREATE TABLE IF NOT EXISTS Replied_To (subscriber varchar(50), thread_id varchar(10), comment_id varchar(10), PRIMARY KEY (subscriber, comment_id))")
 
 #================================= MAIN ========================================
 
 def main():
     reddit = praw.Reddit('bot1')
-    cur.execute("CREATE TABLE IF NOT EXISTS Replied_To (subscriber varchar(50), thread_id varchar(10), comment_id varchar(10))")
+    create_tables()
 
     while True:
-        search_for_delivery(reddit)
+        DeliverySearch(reddit)
 
         print "Sleeping for 30 seconds"
         time.sleep(30)
